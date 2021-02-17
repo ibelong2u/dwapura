d5d1eff1014b29f18b06c89af593b5a2a352ee12
Aditya Shrivastava <ibelong2u+github@gmail.com>
2021-02-17 07:37:20 -0800

    Add files via upload

---
+++ javascript/slide.js
@@ -0,0 +1,104 @@
+/**
+ * Created by QUINOID on 04-01-2018.
+ */
+$(document).ready(function() {
+
+    $("#toggleSwitch_1").hover(
+        function() {
+            $("#theBox_1").slideDown(500);
+        },
+        function() {
+            $("#theBox_1").slideUp(500);
+        });
+
+    $("#toggleSwitch_2").hover(
+        function() {
+            $("#theBox_2").slideDown(500);
+        },
+        function() {
+            $("#theBox_2").slideUp(500);
+        });
+
+    $("#toggleSwitch_3").hover(
+        function() {
+            $("#theBox_3").slideDown(500);
+        },
+        function() {
+            $("#theBox_3").slideUp(500);
+        });
+
+    $("#toggleSwitch_4").hover(
+        function() {
+            $("#theBox_4").slideDown(500);
+        },
+        function() {
+            $("#theBox_4").slideUp(500);
+        });
+
+    $("#toggleSwitch_5").hover(
+        function() {
+            $("#theBox_5").slideDown(500);
+        },
+        function() {
+            $("#theBox_5").slideUp(500);
+        });
+
+    $("#toggleSwitch_6").hover(
+        function() {
+            $("#theBox_6").slideDown(500);
+        },
+        function() {
+            $("#theBox_6").slideUp(500);
+        });
+
+    $("#toggleSwitch_7").hover(
+        function() {
+            $("#theBox_7").slideDown(500);
+        },
+        function() {
+            $("#theBox_7").slideUp(500);
+        });
+
+    $("#toggleSwitch_8").hover(
+        function() {
+            $("#theBox_8").slideDown(500);
+        },
+        function() {
+            $("#theBox_8").slideUp(500);
+        });
+    $("#toggleSwitch_9").hover(
+        function() {
+            $("#theBox_9").slideDown(500);
+        },
+        function() {
+            $("#theBox_9").slideUp(500);
+        });
+    $("#toggleSwitch_10").hover(
+        function() {
+            $("#theBox_10").slideDown(500);
+        },
+        function() {
+            $("#theBox_10").slideUp(500);
+        });
+    $("#toggleSwitch_11").hover(
+        function() {
+            $("#theBox_11").slideDown(500);
+        },
+        function() {
+            $("#theBox_11").slideUp(500);
+        });
+    $("#toggleSwitch_12").hover(
+        function() {
+            $("#theBox_12").slideDown(500);
+        },
+        function() {
+            $("#theBox_12").slideUp(500);
+        });
+    $("#toggleSwitch_13").hover(
+        function() {
+            $("#theBox_13").slideDown(500);
+        },
+        function() {
+            $("#theBox_13").slideUp(500);
+        });
+});
